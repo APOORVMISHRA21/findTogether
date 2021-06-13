a16622141bea9935379ddf07aa02b627fce48173
APOORVMISHRA21 <443_bt19@iiitkalyani.ac.in>
2021-06-13 18:09:59 +0530

    configure mongodb

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const registerRouter = require('./Routes/Register');
 
-mongoose.connect('mongodb+srv://apoorv:apoorv21@findlost.7s1yz.mongodb.net/test',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://apoorv:apoorv21@findlost.7s1yz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.log(err));
 
 let app = express();
 
 app.use(express.json());
-app.use('/Register', registerRouter);
+app.use('/register', registerRouter);
 
 const PORT = process.env.PORT || 3000;
 
