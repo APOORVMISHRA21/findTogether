c39577ea608d828cfdd5b26b6daa5ad7da86fad4
APOORVMISHRA21 <443_bt19@iiitkalyani.ac.in>
2021-06-14 19:02:06 +0530

    login endpoint

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 const registerRouter = require('./Routes/register');
 const loginRouter = require('./Routes/login');
 
-mongoose.connect(DB_URL,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log(err));
-
-// mongoose.connect('mongodb+srv://apoorv:apoorv21@findlost.7s1yz.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect(DB_URL,{useNewUrlParser: true, useUnifiedTopology: true})
 //     .then(() => console.log('Connected to MongoDB...'))
 //     .catch((err) => console.log(err));
+
+mongoose.connect('mongodb+srv://apoorv:apoorv21@findlost.7s1yz.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => console.log(err));
 
 // mongoose.connect('mongodb://localhost:27017/findlost',{useNewUrlParser: true, useUnifiedTopology: true})
 //     .then(() => console.log('Connected to MongoDB...'))
