ca4050ab7dd7bec5f08d7d87d2c4d2cd5c93ea77
APOORVMISHRA21 <443_bt19@iiitkalyani.ac.in>
2021-06-14 18:55:14 +0530

    login endpoint

---
+++ Routes/login.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const express = require('express');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const brcypt = require('bcrypt');
+
+const router = express.Router();
+const { User, validateUser } = require('../Models/User.js');
+
+router.use(express.json());
+
+router.get('/', async (req, res) => {
+    const user = await User.find();
+    res.send(user);
+});
+
+router.post('/', async (req, res) => {
+
+    const { error } = validateCredentials(req.body);
+
+    if(error) return res.status(400).send(error.message);
+
+    const user = await User.findOne({email:req.body.email});
+
+    if(!user) return res.status(400).send("Invalid Email or Password");
+
+    const is_pass = await brcypt.compare(req.body.password, user.password);
+
+    if(!is_pass) return res.status(400).send("Invalid Email or Password");
+
+    //res.send(true);
+    
+    const token = jwt.sign({_id: user._id}, config.get('privateJwtKey'));
+
+    return res.send(token);
+});
+
+const schema = Joi.object({
+    email : Joi.string().required(),
+    password : Joi.string().required()
+});
+
+function validateCredentials(user) {
+    return schema.validate(user);
+}
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
-const registerRouter = require('./Routes/Register');
+const registerRouter = require('./Routes/register');
+const loginRouter = require('./Routes/login');
 
-mongoose.connect('mongodb+srv://apoorv:apoorv21@findlost.7s1yz.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(DB_URL,{useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.log(err));
 
+// mongoose.connect('mongodb+srv://apoorv:apoorv21@findlost.7s1yz.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch((err) => console.log(err));
+
+// mongoose.connect('mongodb://localhost:27017/findlost',{useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch((err) => console.log(err));
+
+
+if(!config.get('privateJwtKey')){
+    console.error('FATAL ERROR, ENV VAR NOT DEFINED');
+    process.exit(1);
+}
 let app = express();
 
 app.use(express.json());
 app.use('/me', (req, res) => {res.send('ONLINE !!!!')});
-app.use('/register', registerRouter);
+app.use('/user', registerRouter);
+app.use('/login', loginRouter);
 
 const PORT = process.env.PORT || 3000;
 
